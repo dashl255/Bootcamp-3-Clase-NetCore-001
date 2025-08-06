97e4fc99ec5fe9d2e0dcadf039f2665e985f76b1
nombre <amirmosan@gmail.com>
2025-08-05 20:27:10 -0500

    Context de base de datos

---
+++ ERP/Bll/User/UserBll.cs
@@ -0,0 +1,19 @@
+﻿using ERP.CoreDB;
+
+namespace ERP.Bll.User
+{
+    public class UserBll : IUserBll
+    {
+        BaseErpContext _context;
+
+        public UserBll(BaseErpContext context)
+        {
+            _context = context;
+        }
+
+        public List<Usuario> GetUsers()
+        {
+            return _context.Usuarios.ToList();
+        }
+    }
+}
---
+++ ERP/Helper/Models/ResponseGeneralModel.cs
@@ -0,0 +1,34 @@
+﻿namespace ERP.Helper.Models
+{
+    public class ResponseGeneralModel<T>
+    {
+        public int code { get; set; }
+        public T data { get; set; }
+        public string message { get; set; }
+        public string messageDev { get; set; }
+
+        public ResponseGeneralModel(int code, string message)
+        {
+            this.code = code;
+            this.message = message;
+        }
+
+        public ResponseGeneralModel(int code, string data, string message, string messageDev)
+        {
+            this.code = code;
+            this.message = message;
+            bool isDebug = (new ConfigurationBuilder()).AddJsonFile("appsettings.json").Build().GetSection("isDebug").Get<bool>();
+            if (isDebug)
+            {
+                this.messageDev = messageDev;
+            }
+        }
+
+        public ResponseGeneralModel(int code, T data, string message)
+        {
+            this.code = code;
+            this.data = data;
+            this.message = message;
+        }
+    }
+}
